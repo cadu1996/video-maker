96c688943439fb14a2d457c3c6bc6fe9aca1ddb5
Carlos Eduardo <carlos.eduardo.gba@gmail.com>
2019-02-26 13:16:45 -0300

    feat: add seatch term and prefix

--- index.py
+++ index.py
@@ -1,13 +1,24 @@
 def start():
     content = {'searchTerm': askAndReturnSearchTerm(),
-               'prefix': askAndReturnPrefix}
+               'prefix': askAndReturnPrefix()}
 
     print(content)
 
 
 def askAndReturnSearchTerm():
-    return raw_input('Type a Wikipedia search term: ')
+    return input('Type a Wikipedia search term: ')
 
 def askAndReturnPrefix():
+    prefixes = ['CANCEL','Who is', 'What is', 'The history of']
+    selectedPrefixIndex = int(input('[1] Whos is\n'
+                     '[2] What is\n'
+                     '[3] The history of\n'
+                     '[0] CANCEL\n'
+                     '\n'
+                     'Choose one option [1, 2, 3, 0]: '))
+
+    selectedPrefixText = prefixes[selectedPrefixIndex]
+
+    return selectedPrefixText
 
 start()
